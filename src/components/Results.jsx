8705879e116b3ba22803356d708e4254971c892e
charlie-wild <charliewild@gmail.com>
2019-02-13 19:59:46 +0000

    refactoring to functional component

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,40 +1,41 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment } from 'react';
 import Collapsible from 'react-collapsible';
 import './results.css';
 
-class Results extends Component {
-  render() {
-    const { results } = this.props;
-    return (
-      <div className="results">
-        {results.length
-          ? (
-            <Fragment>
-              <h3 className="title is-3 is-centered">{results[0].name}</h3>
-              <p className="title is-5">
-                {results[0].description}
+
+const Results = (props) => {
+  const { results } = props;
+  return (
+    <div className="results">
+      {results.length
+        ? (
+          <Fragment>
+            <h3 className="title is-3 is-centered">{results[0].name}</h3>
+            <p className="title is-5">
+              {results[0].description}
+            </p>
+            <p>
+              Click
+              <a href={results[0].html_url}> here </a>
+              to visit the repo, or detailed info to view more information.
+            </p>
+            <Collapsible trigger="Detailed Info">
+              <p>
+                Number of open issues:
+                {results[0].open_issues}
               </p>
               <p>
-                Click
-                <a href={results[0].html_url}> here </a>
-                to visit the repo, or detailed info to view more information.
+                Forks:
+                {results[0].forks}
               </p>
-              <Collapsible trigger="Detailed Info">
-                <p>
-                  Number of open issues:
-                  {results[0].open_issues}
-                </p>
-                <p>
-                  Forks:
-                  {results[0].forks}
-                </p>
-              </Collapsible>
-            </Fragment>
-          )
-          : <p>No Result Found</p>}
-      </div>
-    );
-  }
+            </Collapsible>
+          </Fragment>
+        )
+        : <p>No Result Found</p>}
+    </div>
+  );
 }
 
+
 export default Results;
+

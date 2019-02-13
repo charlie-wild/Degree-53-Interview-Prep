ff0ddaf91bfd3ad169f3b549ee024e927fd29a21
charlie-wild <charliewild@gmail.com>
2019-02-13 15:13:20 +0000

    linting for completed code

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -4,25 +4,36 @@ import Collapsible from 'react-collapsible';
 class Results extends Component {
   render() {
     const { results } = this.props;
-     return (
+    return (
       <div>
-       {results.length ?
-       <Fragment>
-             <h3>{results[0].name}</h3>
-             <p>Click <a href={results[0].html_url}>here </a>to visit the repo.</p>
-             <p>{results[0].description}</p>
-             <Collapsible trigger='Detailed Info'>
-               <p>Placeholder</p>
-             </Collapsible>            
-           
-       </Fragment>
-       
-     : <p>No Result Found</p>}
+        {results.length
+          ? (
+            <Fragment>
+              <h3>{results[0].name}</h3>
+              <p>
+                Click
+                <a href={results[0].html_url}>here </a>
+                to visit the repo.
+              </p>
+              <p>
+                {results[0].description}
+              </p>
+              <Collapsible trigger="Detailed Info">
+                <p>
+                  Number of open issues:
+                  {results[0].open_issues}
+                </p>
+                <p>
+                  Forks:
+                  {results[0].forks}
+                </p>
+              </Collapsible>          
+            </Fragment>
+          )
+          : <p>No Result Found</p>}
       </div>
     );
   }
-
-
 }
 
 export default Results;

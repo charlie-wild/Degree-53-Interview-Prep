1e543b4ef11de6499222e1e92958124c13a594ee
charlie-wild <charliewild@gmail.com>
2019-02-13 14:37:12 +0000

    adding collapsible detailed info

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,4 +1,5 @@
 import React, { Component, Fragment } from 'react';
+import Collapsible from 'react-collapsible';
 
 class Results extends Component {
   render() {
@@ -9,6 +10,10 @@ class Results extends Component {
        <Fragment>
              <h3>{results[0].name}</h3>
              <p>{results[0].description}</p>
+             <Collapsible trigger='Detailed Info'>
+               <p>Placeholder</p>
+             </Collapsible>            
+           
        </Fragment>
        
      : <p>No Result Found</p>}

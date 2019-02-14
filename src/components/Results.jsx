d7bfc352d52dfd431795301da059db8ecbbd74d3
charlie-wild <charliewild@gmail.com>
2019-02-14 11:53:00 +0000

    extracting api.js from components

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -10,27 +10,25 @@ const Results = (props) => {
       {results.length
         ? (
           <Fragment>
-          {results.map((result) => 
-            <h3 className="title is-3 is-centered">{result.name}</h3>
+            <h3 className="title is-3 is-centered">{results[0].name}</h3>
             <p className="title is-5">
-              {results.description}
+              {results[0].description}
             </p>
             <p>
               Click
-              <a href={result.html_url}> here </a>
+              <a href={results[0].html_url}> here </a>
               to visit the repo, or detailed info to view more information.
             </p>
             <Collapsible trigger="Detailed Info">
               <p>
                 Number of open issues:
-                {result.open_issues}
+                {results[0].open_issues}
               </p>
               <p>
                 Forks:
-                {result.forks}
+                {results[0].forks}
               </p>
             </Collapsible>
-          )}
           </Fragment>
         )
         : <p>No Result Found</p>}
@@ -40,3 +38,4 @@ const Results = (props) => {
 
 
 export default Results;
+

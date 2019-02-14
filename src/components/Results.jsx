0b076602054a69f98906290a6bf03ae7e811a733
charlie-wild <charliewild@gmail.com>
2019-02-14 11:50:25 +0000

    removing app.test.js

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -10,25 +10,27 @@ const Results = (props) => {
       {results.length
         ? (
           <Fragment>
-            <h3 className="title is-3 is-centered">{results[0].name}</h3>
+          {results.map((result) => 
+            <h3 className="title is-3 is-centered">{result.name}</h3>
             <p className="title is-5">
-              {results[0].description}
+              {results.description}
             </p>
             <p>
               Click
-              <a href={results[0].html_url}> here </a>
+              <a href={result.html_url}> here </a>
               to visit the repo, or detailed info to view more information.
             </p>
             <Collapsible trigger="Detailed Info">
               <p>
                 Number of open issues:
-                {results[0].open_issues}
+                {result.open_issues}
               </p>
               <p>
                 Forks:
-                {results[0].forks}
+                {result.forks}
               </p>
             </Collapsible>
+          )}
           </Fragment>
         )
         : <p>No Result Found</p>}
